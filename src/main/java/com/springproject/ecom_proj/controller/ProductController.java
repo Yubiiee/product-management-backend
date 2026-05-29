7ebbe175e4e745f96b457029d63fd8cd36fd40c5
Yubika Singh <yubikasingh3108@gmail.com>
2026-05-29 23:54:13 +0530

    Fix CORS origin

--- src/main/java/com/springproject/ecom_proj/controller/ProductController.java
+++ src/main/java/com/springproject/ecom_proj/controller/ProductController.java
@@ -13,7 +13,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "http://localhost:5174")
 @RequestMapping("/api/products")
 public class ProductController {
 
